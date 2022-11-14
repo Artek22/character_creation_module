c41adb1c3d012074ca6fac4586ebd2155eff0542
Андрей <temp60@mail.ru>
2022-11-14 11:54:34 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
-    """Расчет атаки."""
+    # """Расчет атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
